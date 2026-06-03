d2de0e5cc3c7a7181404e5abc284153f1df58415
vondenryan <vonden.ryan@gmail.com>
2026-06-03 20:24:08 -0300

    feat: added argentina

--- src/br/com/agencia/destinos/Argentina.java
+++ src/br/com/agencia/destinos/Argentina.java
@@ -1,2 +1,19 @@
-package br.com.agencia.destinos;public class Argentina {
+package br.com.agencia.destinos;
+
+public class Argentina implements Pais {
+    @Override
+    public String obterNomeMoeda() {
+        return "Peso Argentino (ARS)";
+    }
+
+    @Override
+    public double converterParaMoedaLocal(double valorEmReais) {
+        return valorEmReais * 283.24;
+    }
+
+    @Override
+    public void exibirDicaCultural() {
+        System.out.println("combina a partilha do chimarrão, a paixão pelo futebol e o hábito de saborear o tradicional " +
+                "churrasco ou empanadas em conversas que se estendem até a madrugada.");
+    }
 }
